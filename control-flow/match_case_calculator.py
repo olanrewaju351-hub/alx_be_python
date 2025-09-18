b814ff04b558168113fc5b0a1a4d5c93fe6b8988
Kazeem Olanrewaju Alimi <olanrewaju351@gmail.com>
2025-09-18 16:43:40 -0700

    Add match_case_calculator.py using match/case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -8,17 +8,17 @@ operation = input("Choose the operation (+, -, *, /): ")
 # Use match-case (Python 3.10+)
 match operation:
     case "+":
-        print("Result:", num1 + num2)
+        print(f"The result is {num1 + num2}")
     case "-":
-        print("Result:", num1 - num2)
+        print(f"The result is {num1 - num2}")
     case "*":
-        print("Result:", num1 * num2)
+        print(f"The result is {num1 * num2}")
     case "/":
         if num2 != 0:
-            print("Result:", num1 / num2)
+            print(f"The result is {num1 / num2}")
         else:
             print("Error: Division by zero is not allowed.")
     case _:
-        print("Result: Invalid operation")
+        print("Invalid operation")
 
 
